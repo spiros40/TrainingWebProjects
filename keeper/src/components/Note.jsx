8e3161f26d3ac8861342d3b325a7ea619923305e
spiros40@msn,com <spiros40@msn,com>
2022-03-29 11:51:34 +0300

    react-props

--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Note(){
-    return <div>
+    return <div className="node">
         <h1>Title</h1>
         <p>This is content</p>
     </div>;
---
+++ props-react/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Info from "./Info";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>        
+        <Avatar img={props.img}/>
+      </div>
+        <Info tel={props.tel} email={props.email}/> 
+    </div>
+  );
+}
+
+export default Card;
