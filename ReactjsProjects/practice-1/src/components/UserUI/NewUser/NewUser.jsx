7206e6ce78b95956cc34e09322cad33856a5bde4
spiros40@msn,com <spiros40@msn,com>
2022-05-19 21:25:37 +0300

    react-styling final

---
+++ ReactjsProjects/practice-1/src/components/UserUI/NewUser/NewUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewUser=(props)=>{
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default NewUser;
---
+++ ReactjsProjects/practice-1/src/components/UserUI/UserLogin/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserLogin=(props)=>{
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default UserLogin;
