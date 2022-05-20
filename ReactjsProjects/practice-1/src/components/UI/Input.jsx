23a5ed320e0d7bec29278304a7f53e9a5afdd660
spiros <spiros40@msn.com>
2022-05-20 21:24:25 +0300

    practice-1,1

--- ReactjsProjects/practice-1/src/components/UI/Input.jsx
+++ ReactjsProjects/practice-1/src/components/UI/Input.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Input=(props)=>{
     return (
         <div>
-
+            <label>User Name</label>
+            <input type="text" />
+            <label>Age</label>
+            <input type="text" />
         </div>
     );
 }
