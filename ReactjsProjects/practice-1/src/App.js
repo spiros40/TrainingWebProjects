985a68a46029994d6a6fa991b561c712e448b56f
spiros <spiros40@msn.com>
2022-05-21 09:19:15 +0300

    practice 1,2

--- ReactjsProjects/practice-1/src/App.js
+++ ReactjsProjects/practice-1/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Button from "./components/UI/Button";
 
 function App() {
   return (
     <div>
-    
+      <Button name={"Add"}/>
     </div>
   );
 }
--- ReactjsProjects/practice-1/src/components/UI/Button.jsx
+++ ReactjsProjects/practice-1/src/components/UI/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./Button.css";
 
 const Button=(props)=>{
     return(
         <div>
-            
+            <button> {props.name}</button>
         </div>
     );
 
